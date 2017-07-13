26a94a64901dbdfa61f97717aff3b3054f045857
UndreamtMayhem <danny_boyx@hotmail.co.uk>
2017-07-13 15:58:06 +0100

    Update engine.js

--- P3 ARCADE GAME Frogger/js/engine.js
+++ P3 ARCADE GAME Frogger/js/engine.js
@@ -137,12 +137,12 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'http://res.cloudinary.com/undreamtmayhem/image/upload/v1499957411/frogger-assests/water-block.png',   // Top row is water
+                'http://res.cloudinary.com/undreamtmayhem/image/upload/v1499957408/frogger-assests/stone-block.png',   // Row 1 of 3 of stone
+                'http://res.cloudinary.com/undreamtmayhem/image/upload/v1499957408/frogger-assests/stone-block.png',   // Row 2 of 3 of stone
+                'http://res.cloudinary.com/undreamtmayhem/image/upload/v1499957408/frogger-assests/stone-block.png',   // Row 3 of 3 of stone
+                'http://res.cloudinary.com/undreamtmayhem/image/upload/v1499957408/frogger-assests/grass-block.png',   // Row 1 of 2 of grass
+                'http://res.cloudinary.com/undreamtmayhem/image/upload/v1499957408/frogger-assests/grass-block.png'    // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
